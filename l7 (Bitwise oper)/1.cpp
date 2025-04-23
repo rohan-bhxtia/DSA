c78b318309f62de1ac7ee28581b940af867ac5c9
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-04-23 14:30:45 +0530

    ..

---
+++ l7 (Bitwise oper)/1.cpp
@@ -0,0 +1,12 @@
+// i performed and , or , and xor operator on same numbers ... ( &,| , ^)
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a ,b = (6,10);
+    cout<<(6&10)<<endl;   //bitwise and op 
+    cout<<(6|10)<<endl;   //bitwise or op
+    cout<<(6^10)<<endl;   //bitwise xor op
+    return 0;
+}
---
+++ l7 (Bitwise oper)/2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int a , b = (10,2);
+    cout<< ( 10<<2 )<<endl;   // bitwise << operator ( left shift )
+    cout<< (10>>1) <<endl;      //bitwise >> operator (right shift)
+    return 0;
+}
