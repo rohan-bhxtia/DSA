5a98a0251df21e7c56ef04c952d81af5fc0d39b5
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-04-27 17:15:08 +0530

    ..

--- L8 Array-1/practice ques/3.cpp
+++ L8 Array-1/practice ques/3.cpp
@@ -1,28 +1,35 @@
 //WAF to ptint all the unique values in an array 
 
+
+
 #include <iostream>
 using namespace std;
 
-void unique(int arr[],int sz){
-    for(int i=0;i<sz;i++){
-        bool isUnique =true;
-        for(int j=1;j<sz;j++){
-            if(i!=j && arr[i]==arr[j]){
-                isUnique=false;
-                break;
-            }
-            else{
-                cout<<arr[i]<<" ";
+// Function to print all unique elements
+void unique(int arr[], int sz) {
+    for (int i = 0; i < sz; i++) {
+        int count = 0; // count how many times arr[i] appears
+
+        // Check arr[i] with every element
+        for (int j = 0; j < sz; j++) {
+            if (arr[i] == arr[j]) {
+                count++; // increase count if match found
             }
         }
-        
-    }
-    cout<<endl;
 
+        // If count is 1, it means it appears only once (unique)
+        if (count == 1) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
 }
-int main(){
-    int arr[]={1,2,1,2,3};
+
+int main() {
+    int arr[] = {1, 2, 1, 2, 3};
     int sz = 5;
-    unique(arr,sz);
+
+    unique(arr, sz);
+
     return 0;
 }
