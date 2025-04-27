08a44ac3cf96609b471d5e688718ccdd3a7ffb7f
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-04-27 14:43:43 +0530

    ..

--- L8 Array-1/5.cpp
+++ L8 Array-1/5.cpp
@@ -2,20 +2,28 @@
 
 #include <iostream>
 using namespace std;
-
+                                                           // 2 pointer approach 
  
-int reverse_arr(int arr[],int size){
+void reverse_arr(int arr[],int size){
     int start =0;
     int end = size-1;
     while(start<end){
         swap(arr[start],arr[end]);
         start++;
         end--;
-        
-        
     }
-    
+}
 
+    int main(){
+        int arr[]= {2,5,4,6,4,3,7,4};
+        int size =8;
+        reverse_arr(arr,size);
+
+        for(int i=0; i<size;i++){
+            cout<< arr[i]<<" ";
+        }
+        cout<<endl;
+        return 0;
     }
     
    
