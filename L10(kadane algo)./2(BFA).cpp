df1bcaddb13c3dc2a31c9bd2ac746d35182f6014
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-05-03 13:50:04 +0530

    ..

--- L10(kadane algo)./2(BFA).cpp
+++ L10(kadane algo)./2(BFA).cpp
@@ -2,18 +2,23 @@
 // Maximum subarray sum
  
 #include <iostream>
+#include <climits>
 using namespace std ;
 int main(){
     int n=5 ;
     int arr[5]={1,2,3,4,5};
+
+    int max_sum =INT_MIN; 
+
     for( int st=0 ; st<n ; st++){
         int currentSum =0;
-        for(int end=st ; st <n ; st++){
+        for(int end=st ; end <n ; end++){
             currentSum += arr[end];
+            max_sum = max(currentSum,max_sum);
         }
-
     }
-    cout<< currentSum<<endl;
+
+    cout<< max_sum<<endl;
     return 0;
     
 
