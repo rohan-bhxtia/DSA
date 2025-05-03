fe56abf36d0a7b1cde5e520c9a4fadfd914ced78
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-05-03 15:03:02 +0530

    ..

--- L10(kadane algo)./3(kadane).cpp
+++ L10(kadane algo)./3(kadane).cpp
@@ -1,1 +1,24 @@
+  #include <iostream>
+  #include <climits>
+  using namespace std ;
+
+  int main(){
+
+    int arr[7]={3,-4,5,4,-1,7,-8};
+    int n =7;
+
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int st = 0 ; st<n ;st++){
+        currentSum += arr[st];
+        maxSum = max(currentSum,maxSum);
+        if(currentSum<0){
+            currentSum =0;
+        }
+    }
+    cout<<maxSum<<endl;
+
   
+  return 0;
+  }
--- practice /prac.cpp
+++ practice /prac.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <climits>
 using namespace std ;
 
 int main(){
-    int arr[5] = {1,2,3,4,5};
+    int arr[5] = {-1,2,3,4,-5};
     int n = 5;
 
+    int maxSum = INT_MIN;
     
-}
+    for(int st =0 ; st<n ;st++){
+        int currentSum = 0;
+        for(int end = st ; end<n ;end++){
+            currentSum += arr[end];
+            maxSum = max(currentSum,maxSum);
+        }        
+    }
+    cout<<maxSum<<endl;
+    return 0;
+}  
