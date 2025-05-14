fc63b9ca14ee09d381a723912e31c160505de54b
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-05-14 21:51:42 +0530

    ..

--- L9 (vectors)/3(leetcode ).cpp
+++ L9 (vectors)/3(leetcode ).cpp
@@ -6,7 +6,7 @@ using namespace std;
 void unique(int nums[], int sz){
     for(int i=0;i<sz;i++){
          bool isUnique =true;
-        for(int j=0;j<sz;j++){                  //bhan di ln 
+        for(int j=0;j<sz;j++){                  
             if(i != j && nums[i]==nums[j]){
                 isUnique = false;
                 break;
@@ -19,7 +19,7 @@ void unique(int nums[], int sz){
     }
 }
 int main(){
-int arr[]={1,2,3,4,4,3,2,1,1,2,3,4,4,3,2,1,8};
+int arr[]={1,2,3,7,7,3,2,1,1,2,3,4,4,3,2,1,8};
 int sz =17;
 unique(arr,sz);
 return 0;
--- practice /prac.cpp
+++ practice /prac.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
+
 int main(){
-    int n=4 ;
-    for(int i =1 ; i<=n;i++){
-        //for space 
-        for(int j=n;j>=i;j--){
-            cout<<" ";
-        }
-        //for numbers first half pyramid
-        for(int j = 1;j<=i;j++){   
-            cout<<j;
-        }
-        //for numbers second half pyramind
-        for(int j=i-1;j>=1;j--){
-            cout<<j;
-        }
-        cout<<endl;
+    int arr[5]={1,2,2,3,1};
+    int sz = 5;
+    int sum = 0;
+    for(int i=0;i<sz;i++){
+        arr[i]^
+
     }
-    return 0;
 }
